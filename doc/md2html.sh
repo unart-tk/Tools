eb536198aef3b22bf402ffd2eb52b00d0db2557f
Ben <ben@lekuin.(none)>
2011-02-11 12:30:12 +0100

    upd

--- doc/md2html.sh
+++ doc/md2html.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-# md2html - convert markdown to html
-# ==================================
+# md2html - Alias for markdown conversion
+# =======================================
 #
-#     (c) 2011 Ben <b@untar.org>
+#     (c) 2011 Ben, untar.org>
 # 
 
 set -e
 
-pandoc -s -f markdown -t html "$@"
+fmd=''     # input/ markdown
+fhtml=''   # output/html
+
+# conversion command
+cmd='pandoc -s  -- "$fhtml" > "$fmd"'
+
+if [ "$#" = 1 ];then
+  fhtml="$1"
+else
+  echo "Usage: $0 - <markdown file>"
+  exit 1
+fi
+
+fbn=$(basename "$fhtml")
+fn=$(echo "$fbn" | sed 's/\..*//g')
+fmd="${fn}.html"
+
+eval ${cmd}
